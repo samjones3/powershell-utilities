56edaf660286dbe19c1cf9ce658e6255b129860e
Rafael <rafael@bogus-email.com>
2021-12-28 14:00:01 -0800

    enhancing comments...

--- regex-unit-tests.ps1
+++ regex-unit-tests.ps1
@@ -2,6 +2,10 @@
 
 # This uses the Pester framework. Note you have to update to Pester
 # version 5.x (Win10 comes with Pester 3.x which has different syntax)
+
+# Note that literals in non latin letters must be in double quotes
+# (this is a powershell thing). Also, the .ps1 must have a BOM
+
 Describe "regex validation suite" {
     It "first test" {
         $string = 'test'
